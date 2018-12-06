47f4a335177216e9daf6a3e09a94b539f6c1df6a
edwardGH7 <35962131+edwardGH123@users.noreply.github.com>
2018-12-06 21:19:50 +0800

    Update TestController.java

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test(){
-        return "Hello World!!";
+        return "Hello World!!!!!";
     }
 }
